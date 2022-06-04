0d51f4a52456dd3ddb3119165002fc00e86a3876
Chelsea Katsidzira <ckatsidzira322@gmail.com>
2022-06-04 19:04:49 -0400

    created timer screen and custom count down timer

---
+++ app/src/main/java/com/example/timer/ui/screens/TimerScreen.kt
@@ -0,0 +1,40 @@
+package com.example.timer.ui.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.timer.ui.components.CountDownIndicator
+
+@Composable
+fun TimerScreen(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        CountDownIndicator(
+            totalTime = 100L * 1000L,
+            handleColor = Color.Green,
+            inactiveBarColor = Color.DarkGray,
+            activeBarColor = Color(0xFF37B900),
+            modifier = Modifier.size(200.dp)
+        )
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewTimerScreen() {
+    TimerScreen()
+}
